811805cbfba618f4e1f30f3865154d3d17a99e75
QubitTooLate <jelmer.bonne00@gmail.com>
2020-11-02 13:07:29 +0100

    Hello World

---
+++ OutputConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OutputConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
